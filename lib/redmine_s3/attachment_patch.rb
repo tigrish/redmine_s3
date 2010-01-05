72a11147c4faba3af59672e641d1f0f1a2b36dbd
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-05 09:46:29 +0800

    Obscure the uploaded filename a bit by putting it into a "directory".

--- lib/redmine_s3/attachment_patch.rb
+++ lib/redmine_s3/attachment_patch.rb
@@ -19,9 +19,9 @@ module RedmineS3
     module InstanceMethods
       def put_to_s3
         if @temp_file && (@temp_file.size > 0)
-          logger.debug("Uploading to #{RedmineS3::Connection.uri}/#{disk_filename}")
-          RedmineS3::Connection.put(disk_filename, @temp_file.read)
-          RedmineS3::Connection.publicly_readable!(disk_filename)
+          logger.debug("Uploading to #{RedmineS3::Connection.uri}/#{path_to_file}")
+          RedmineS3::Connection.put(path_to_file, @temp_file.read)
+          RedmineS3::Connection.publicly_readable!(path_to_file)
           md5 = Digest::MD5.new
           self.digest = md5.hexdigest
         end
@@ -29,8 +29,13 @@ module RedmineS3
       end
 
       def delete_from_s3
-        logger.debug("Deleting #{RedmineS3::Connection.uri}/#{disk_filename}")
-        RedmineS3::Connection.delete(disk_filename)
+        logger.debug("Deleting #{RedmineS3::Connection.uri}/#{path_to_file}")
+        RedmineS3::Connection.delete(path_to_file)
+      end
+
+      def path_to_file
+        # obscure the filename by using the timestamp for the 'directory'
+        disk_filename.split('_').first + '/' + disk_filename
       end
     end
   end
